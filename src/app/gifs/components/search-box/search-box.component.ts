0b469268dcfd8284d7486af6d7372de09d92f673
Jose Luis <bosito321@gmail.com>
2024-04-20 17:12:48 -0500

    first part to Angular course

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-box',
+  templateUrl: './search-box.component.html',
+  styleUrls: ['./search-box.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SearchBoxComponent implements OnInit {
+  ngOnInit(): void {}
+}
---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent implements OnInit {
+  ngOnInit(): void {}
+}
